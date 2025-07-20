7539bde09b075d9fa2413fc2d9b76bd45db8b3f1
Rickie Karp <rickie.karp@yandex.com>
2025-07-20 16:37:08 +0200

    Add more logging to backup script

--- backup/dev/backup-db.sh
+++ backup/dev/backup-db.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
+dataDir="./data"
+
+if [ -d $dataDir ]; then
+    echo "Directory $dataDir found, removing..."
+    rm -rf $dataDir
+fi
+
 mkdir data
 
 # --------------------------
 # create database backups
 # --------------------------
-docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases login > ./data/login.sql
-docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases nexus > ./data/nexus.sql
-docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases storage > ./data/storage.sql
-docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases mysql > ./data/mysql.sql
+echo "Dumping databases"
+docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases login > $dataDir/login.sql
+docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases nexus > $dataDir/nexus.sql
+docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases storage > $dataDir/storage.sql
+docker exec -it database mariadb-dump --defaults-file="/home/defaults.cnf" --routines --databases mysql > $dataDir/mysql.sql
 
 # --------------------------
 # compress database backups
 # --------------------------
-tar cJPf ./data/dumps.tar.xz ./data/*.sql
-rm ./data/*.sql
+echo "Compressing dump"
+tar cJPf $dataDir/dumps.tar.xz $dataDir/*.sql
+rm $dataDir/*.sql
 
 # --------------------------
 # encrypt database backups
 # --------------------------
-gpg --output ./data/dumps.tar.xz.gpg --encrypt --recipient rickie.karp@yandex.com ./data/dumps.tar.xz
+echo "Encrypting for recipient: $DEFAULT_EMAIL"
+gpg --output $dataDir/dumps.tar.xz.gpg --encrypt --recipient $DEFAULT_EMAIL $dataDir/dumps.tar.xz
 
 # --------------------------
 # sync database backups
 # --------------------------
-rsync -rlvpt ./data/dumps.tar.xz.gpg pi:/mnt/raid2/archive/database/development/
+echo "Syncing backup"
+rsync -rlvpt $dataDir/dumps.tar.xz.gpg pi:/mnt/raid2/archive/database/development/
 
 # --------------------------
 # remove database backups
